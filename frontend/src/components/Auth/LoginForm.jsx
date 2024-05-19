7eb674931c91734bf66ed91fe5c7b22fb5ced69b
ShamnaNoushad <shamna947n@gmail.com>
2024-05-19 11:54:43 +0530

    frontend-design

--- frontend/src/components/Auth/LoginForm.jsx
+++ frontend/src/components/Auth/LoginForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import logo1 from '../assets/chat.png'
+import './auth.css'
 
 function LoginForm() {
     const [email, setEmail] = useState('');
@@ -22,25 +24,28 @@ function LoginForm() {
     };
 
     return (
-        <div style={{ padding: '1.5rem', maxWidth: '32rem', margin: 'auto', marginTop: '10%' }}>
-            <h1 style={{ fontSize: '1.875rem', textAlign: 'center', fontWeight: '600', marginTop: '1.75rem' }}>Login</h1>
 
-            <form onSubmit={handleLogin} style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-                <input type="email" placeholder="Email" style={{ border: '1px solid #000', padding: '0.75rem', borderRadius: '0.5rem' }} id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="Password" style={{ border: '1px solid #000', padding: '0.75rem', borderRadius: '0.5rem' }} id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit" style={{ backgroundColor: '#374151', color: '#fff', padding: '0.75rem', borderRadius: '0.5rem', textTransform: 'uppercase', cursor: 'pointer', transition: 'opacity 0.3s' }} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-                    Sign In
-                </button>
-            </form>
-
-            <div style={{ display: 'flex', gap: '0.5rem', marginTop: '1.25rem' }}>
-                <p>Don't have an account?</p>
-                <Link to={'/register'}>
-                    <span style={{ color: '#3182ce', cursor: 'pointer' }}>Register</span>
-                </Link>
+        <div>
+            <div className='frm'>
+                <form onSubmit={handleLogin}>
+                    <div className='brand'>
+                        <img src={logo1} alt="logo" />
+                        <h1>Chatify</h1>
+                    </div>
+                    <input type="email" placeholder="Email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button type='submit'>Login</button>
+                </form>
+                <div style={{ display: 'flex', gap: '0.5rem', marginTop: '1.25rem' }}>
+                    <p>Don't have an account?</p>
+                    <Link to={'/register'}>
+                        <span style={{ color: '#3182ce', cursor: 'pointer' }}>Register</span>
+                    </Link>
+                </div>
+                {errorMessage && <p style={{ color: 'red', marginTop: '0.5rem' }}>{errorMessage}</p>}
             </div>
-            {errorMessage && <p style={{ color: 'red', marginTop: '0.5rem' }}>{errorMessage}</p>}
         </div>
+
     );
 }
 
--- frontend/src/components/Auth/RegisterForm.jsx
+++ frontend/src/components/Auth/RegisterForm.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import logo1 from '../assets/chat.png'
+import './auth.css'
 
 function RegisterForm() {
     const [username, setUsername] = useState('');
@@ -22,25 +24,29 @@ function RegisterForm() {
     };
 
     return (
-        <div style={{ padding: '1.5rem', maxWidth: '32rem', margin: 'auto', marginTop: '10%' }}>
-            <h1 style={{ fontSize: '1.875rem', textAlign: 'center', fontWeight: '600', marginTop: '1.75rem' }}>Sign Up</h1>
-            <form onSubmit={handleSignUp} style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-                <input type="text" placeholder="Username" style={{ border: '1px solid #000', padding: '0.75rem', borderRadius: '0.5rem' }} id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                <input type="email" placeholder="Email" style={{ border: '1px solid #000', padding: '0.75rem', borderRadius: '0.5rem' }} id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="Password" style={{ border: '1px solid #000', padding: '0.75rem', borderRadius: '0.5rem' }} id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit" style={{ backgroundColor: '#374151', color: '#fff', padding: '0.75rem', borderRadius: '0.5rem', textTransform: 'uppercase', cursor: 'pointer', transition: 'opacity 0.3s' }} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-                    Register
-                </button>
-            </form>
-            <div style={{ display: 'flex', gap: '0.5rem', marginTop: '1.25rem' }}>
-                <p>Already have an account?</p>
-                <Link to={'/'}>
-                    <span style={{ color: '#3182ce', cursor: 'pointer' }}>Login</span>
-                </Link>
+
+            <div>
+                <div className='frm'>
+                    <form onSubmit={handleSignUp}>
+                        <div className='brand'>
+                            <img src={logo1} alt="logo" />
+                            <h1>Chatify</h1>
+                        </div>
+                        <input type="text" placeholder="Username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <input type="email" placeholder="Email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" placeholder="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <button type='submit'>Create User</button>
+                    </form>
+                    <div style={{ display: 'flex', gap: '0.5rem', marginTop: '1.25rem' }}>
+                        <p>Already have an account?</p>
+                        <Link to={'/'}>
+                            <span style={{ color: '#3182ce', cursor: 'pointer' }}>Login</span>
+                        </Link>
+                    </div>
+                    {errorMessage && <p style={{ color: 'red', marginTop: '0.5rem' }}>{errorMessage}</p>}
+                </div>
             </div>
-            {errorMessage && <p style={{ color: 'red', marginTop: '0.5rem' }}>{errorMessage}</p>}
-        </div>
-    );
+            );
 }
 
 export default RegisterForm
